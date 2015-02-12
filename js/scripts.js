2818f26eb22eb1e86733e3692c347c62997f73f1
Dex Fitch and Mike Goren <fitch.dexter@gmail.com>
2015-02-12 14:19:10 -0800

    Add frontend and do minor refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,40 @@
-var baseChanger = function(decimal) {
-  var digits = decimal.split("").reverse();
+var baseChanger = function(number, base) {
+  var conversion = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
+  '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15};
+
+  var digits = number.toUpperCase().split("").reverse();
   var total = 0;
 
   digits.forEach(function(digit, index) {
-    total += parseInt(digit) * Math.pow(2, index);
+
+    total += conversion[digit] * Math.pow(base, index);
   });
 
   return total;
 };
+
+// ----------------------------------------------------
+
+$(document).ready(function() {
+
+  $("form#base-jumper").submit(function(event) {
+    $("#decimal").text("");
+    $("#result").hide();
+    $("#error").hide();
+
+    var num = $("input#number").val();
+    var base = $("input#base").val();
+    var dec = baseChanger(num, base);
+
+    $("#decimal").text(dec)
+
+    if (dec) {
+      $("#result").show();
+    } else {
+      $("#error").show();
+    }
+
+    event.preventDefault();
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,19 @@
 describe("baseChanger", function() {
 
   it("returns decimal equivalent of binary 2's", function() {
-    expect(baseChanger("10")).to.equal(2);
+    expect(baseChanger("10", 2)).to.equal(2);
   });
+
   it("returns decimal equivalent of binary for any number", function() {
-    expect(baseChanger("11101010")).to.equal(234);
+    expect(baseChanger("11101010", 2)).to.equal(234);
+  });
+
+  it("returns decimal equivalent of octal for any number", function() {
+    expect(baseChanger("174", 8)).to.equal(124);
+  });
+
+  it("returns decimal equivalent of hex for any number", function() {
+    expect(baseChanger("7C", 16)).to.equal(124);
   });
 
 });
